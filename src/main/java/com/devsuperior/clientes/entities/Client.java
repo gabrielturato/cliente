bade36fe6ac4eca1a4554e5d6acf3a9a3543dea8
Gabriel Turato <gabrielturato@outlook.com>
2025-01-23 17:16:56 -0300

    Seed de banco de dados

--- src/main/java/com/devsuperior/clientes/entities/Client.java
+++ src/main/java/com/devsuperior/clientes/entities/Client.java
@@ -10,7 +10,7 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
+    private String name;
     private String cpf;
     private Double income;
     private LocalDate birthDate;
@@ -18,7 +18,7 @@ public class Client {
 
     public Client(Long id, String nome, String cpf, Double income, LocalDate birthDate, Integer children) {
         this.id = id;
-        this.nome = nome;
+        this.name = nome;
         this.cpf = cpf;
         this.income = income;
         this.birthDate = birthDate;
@@ -35,12 +35,12 @@ public class Client {
         this.id = id;
     }
 
-    public String getNome() {
-        return nome;
+    public String getName() {
+        return name;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public Double getIncome() {
