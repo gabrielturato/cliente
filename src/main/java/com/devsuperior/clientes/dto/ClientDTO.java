2425be25f7a5d492d4d0fdcf4f206639b6ee0a6a
Gabriel Turato <gabrielturato@outlook.com>
2025-01-23 17:30:49 -0300

    Criacao do DTO

---
+++ src/main/java/com/devsuperior/clientes/dto/ClientDTO.java
@@ -0,0 +1,34 @@
+package com.devsuperior.clientes.dto;
+
+import com.devsuperior.clientes.entities.Client;
+
+import java.time.LocalDate;
+
+public class ClientDTO {
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private LocalDate birthDate;
+    private Integer children;
+
+    public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public ClientDTO(Client client){
+        id = client.getId();
+        name = client.getName();
+        cpf = client.getCpf();
+        income = client.getIncome();
+        birthDate = client.getBirthDate();
+        children = client.getChildren();
+    }
+
+}
---
+++ src/main/java/com/devsuperior/clientes/services/ClientService.java
@@ -0,0 +1,4 @@
+package com.devsuperior.clientes.services;
+
+public class ClientService {
+}
